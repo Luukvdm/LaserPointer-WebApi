6e7424ff5ddb8139c6cbbeced10f6371d783a025
Luuk van den Maagdenberg <luuk.vdm@hotmail.com>
2020-10-24 19:38:57 +0200

    Db init migration

---
+++ src/WebApi/Infrastructure/Persistence/Migrations/20201022202604_InitCreate.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LaserPointer.WebApi.Infrastructure.Persistence.Migrations
+{
+    public partial class InitCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jobs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Created = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    LastModified = table.Column<DateTime>(nullable: true),
+                    LastModifiedBy = table.Column<string>(nullable: true),
+                    HashType = table.Column<string>(nullable: false),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jobs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hashes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    JobId = table.Column<int>(nullable: true),
+                    Value = table.Column<byte[]>(nullable: false),
+                    PlainValue = table.Column<string>(maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hashes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Hashes_Jobs_JobId",
+                        column: x => x.JobId,
+                        principalTable: "Jobs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Hashes_JobId",
+                table: "Hashes",
+                column: "JobId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Hashes");
+
+            migrationBuilder.DropTable(
+                name: "Jobs");
+        }
+    }
+}
